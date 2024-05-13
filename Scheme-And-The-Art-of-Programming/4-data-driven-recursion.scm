e3351de23363bd231d14498a489466c1a3f48616
Davies Magare <davieskamanda@gmail.com>
2024-05-13 05:32:01 +0000

    Add iterative and recursive definitions of occurs function

--- Scheme-And-The-Art-of-Programming/4-data-driven-recursion.scm
+++ Scheme-And-The-Art-of-Programming/4-data-driven-recursion.scm
@@ -45,3 +45,40 @@
                                      (mk-desc-helper (sub1 n) (cons acc (list n)))))))
         (mk-desc-helper n '()))))
 
+
+;; occurs - count the top level occurences of item in a list
+;; @item: The item whose number of occurences to count
+;; @ls: The list to count occurences of item
+;; helper-procedures: none
+;; Return: The integer occurences of item in ls
+(define occurs
+    (lambda (item ls)
+      (cond
+        ((null? ls) 0)
+        (else
+          (if (equal? (car ls) item)
+              (add1 (occurs item (cdr ls)))
+              (occurs item (cdr ls)))))))
+
+;; occurs-it - Count occurences of an item in a list iteratively
+;; @item: The item to count
+;; @list: The list in which to count item
+;;     helper-procedures: occurs-it-helper
+;;     @occurs-it-helper - Helper procedure to occurs-it
+;;     @ls: A list to count the number of occurences of item
+;;     @acc: An accumulator to store the results in each iteration
+;;     Return: A procedure that counts the number of occurences of item in ls
+;; Return: Number of occurences of item in ls
+
+(define occurs-it
+    (lambda (item ls)
+      (letrec ((occurs-it-helper (lambda (ls acc)
+                                   (cond
+                                     ((null? ls) acc)
+                                     (else
+                                       (if (equal? (car ls) item)
+                                           (occurs-it-helper (cdr ls) (add1 acc))
+                                           (occurs-it-helper (cdr ls) acc)))))))
+        (occurs-it-helper ls 0))))
+
+
